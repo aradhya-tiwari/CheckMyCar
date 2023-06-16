a774bbb4f6a51a34ec322399042d0750a07c3fe5
Nikita Parmar <nikitaparmar7112@gmail.com>
2023-06-16 16:01:04 +0530

    contactus and faqs

---
+++ src/routes/faqs/+page.svelte
@@ -0,0 +1,131 @@
+<style>
+    .box1{
+        background-color: rgb(69,64,64);
+        height: 100%;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+    }
+    .head1{
+        color:white;
+        font-size: 3rem;
+        font-weight: bold;
+        font-family: serif;
+        padding-left: 4.5rem;
+        margin-top: 2.3rem;
+        margin-bottom:1.5rem;
+    }
+    .faqsec{
+        background-color: rgb(214,193,193);
+        width: 865px;
+        height: 1953px;
+        margin-top: 1rem;
+        margin-left: 6rem;
+        margin-bottom: 2.5rem;
+    }
+    .msgdrop{
+        width: 100%;
+        height: 27rem;
+        margin-top: 2rem;
+        background-color: rgb(134,134,139);
+        display: flex;
+        flex-direction: column;
+    }
+    .qu1{
+        margin-top: 2rem;
+    }
+    .ques{
+       font-family: sans-serif;
+       padding: 0rem 2rem 0.2rem 1.5rem;
+       font-size: 2rem;
+       font-style: revert;
+    }
+    .ans{
+        font-family: sans-serif;
+        padding:0rem 2rem 2rem 0.5rem ;
+        font-size: 1.5rem;
+        border-bottom: .1rem solid white;
+        font-style: revert;
+    }
+    .fe1{
+        font-size: 2rem;
+        padding-left: 6rem;
+        padding-top: 1rem;
+    } 
+    .icon1{
+        padding-left: 6rem;
+    }
+    .fa-regular{
+        font-size:1.5rem;
+        margin-right: .75rem;
+        margin-top: .5rem;
+    }
+   
+   .fe2{
+      width: 32rem;
+      margin-top: 1.5rem;
+      font-size: 1.5rem;
+      font-weight: lighter;
+      padding-left: 6rem;
+   }
+   input[type="submit"]
+    {
+    margin-bottom: 2rem; 
+    margin-top: 1rem;
+    border-radius: 2px;
+    width: 9.5rem;
+    background-color: rgb(21,97,240);
+    margin-left: 6rem;
+    border-color: transparent;
+    color:white;
+    font-weight: bold;
+    }
+    input[type="submit"]:hover{
+        background-color: #2691d9;
+        border-color:rgb(47, 41, 37);
+    }
+    textarea{    
+    width: 50rem;
+    height: 15rem;
+    margin-left: 6rem;
+    padding-top: 0.5rem;
+    padding-left: 0.5rem;
+    border-radius: 0.5rem;
+    font-size: 1.25rem;
+    }
+</style>
+
+
+
+<div class ="box1">
+    <div class="head1">Frequently Asked Questions</div>
+    <div class="faqsec">
+        <div class="ques qu1">Q)Duis imperdiet erat non sollicitudin ultricies, in fermentum felis?</div>
+        <div class="ans">->Duis imperdiet erat non sollicitudin ultricies. Proin et cursus eros, in fermentum felis.</div>
+        <div class="ques">Q)Duis imperdiet erat non sollicitudin ultricies, in fermentum felis?</div>
+        <div class="ans">->Duis imperdiet erat non sollicitudin ultricies. Proin et cursus eros, in fermentum felis.</div>
+        <div class="ques">Q)Duis imperdiet erat non sollicitudin ultricies, in fermentum felis?</div>
+        <div class="ans">->Duis imperdiet erat non sollicitudin ultricies. Proin et cursus eros, in fermentum felis.</div>
+        <div class="ques">Q)Duis imperdiet erat non sollicitudin ultricies, in fermentum felis?</div>
+        <div class="ans">->Duis imperdiet erat non sollicitudin ultricies. Proin et cursus eros, in fermentum felis.</div>
+        <div class="ques">Q)Duis imperdiet erat non sollicitudin ultricies, in fermentum felis?</div>
+        <div class="ans">->Duis imperdiet erat non sollicitudin ultricies. Proin et cursus eros, in fermentum felis.</div>
+        <div class="ques">Q)Duis imperdiet erat non sollicitudin ultricies, in fermentum felis?</div>
+        <div class="ans">->Duis imperdiet erat non sollicitudin ultricies. Proin et cursus eros, in fermentum felis.</div>
+        <div class="ques">Q)Duis imperdiet erat non sollicitudin ultricies, in fermentum felis?</div>
+        <div class="ans">->Duis imperdiet erat non sollicitudin ultricies. Proin et cursus eros, in fermentum felis.</div>
+        <div class="ques">Q)Duis imperdiet erat non sollicitudin ultricies, in fermentum felis?</div>
+        <div class="ans">->Duis imperdiet erat non sollicitudin ultricies. Proin et cursus eros, in fermentum felis.</div>
+        <div class="ques">Q)Duis imperdiet erat non sollicitudin ultricies, in fermentum felis?</div>
+        <div class="ans">-> Duis imperdiet erat non sollicitudin ultricies. Proin et cursus eros, 
+            in fermentum felisDuis imperdiet erat non sollicitudin ultricies. Proin et cursus eros, in fermentum felis.</div>
+    </div>
+    <div class="msgdrop">
+        <div class="fe1">Got Your Answers ?</div>
+        <div class="icon1"><i class="fa-regular fa-thumbs-up"></i>
+         <i class="fa-regular fa-thumbs-down fa-flip-horizontal"></i></div>
+        <div class="fe2">If This Doesn't Seem To Solve Your Query Then Send your Question!</div>
+        <textarea name="typ"  placeholder="Your Question..."></textarea>
+        <input type="submit" value="Submit">
+    </div>
+</div>
