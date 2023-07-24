15a225a95a3eea24c61e2f23b84a2949028bbb77
aradhyatiwari <aradhya.prad@gmail.com>
2023-07-24 22:52:54 +0530

    trying netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from "@sveltejs/kit/vite";
-import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from "@sveltejs/adapter-netlify";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,10 +8,7 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      routes: {
-        include: ['/*'],
-        exclude: ['<all>']
-      }
+
     }),
   },
 
