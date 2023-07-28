f30ff6c26a0a290e9d1a48716d3703736501f241
aradhyatiwari <aradhya.prad@gmail.com>
2023-07-28 06:55:07 +0530

    drop down added

--- src/lib/components/Dropdown.svelte
+++ src/lib/components/Dropdown.svelte
@@ -1,19 +1,19 @@
 <script>
-    import { fade } from 'svelte/transition';
+    import { fly } from 'svelte/transition';
     let _toggle = false
     export let text = "Button"
     function toggle(){
         _toggle = !_toggle
     }
 </script>
-<div class="relative " tran>
+<div class="relative w-fit">
 
-    <button on:click={toggle} class="px-5 py-2 rounded-lg bg-[#86868B] text-white hover:shadow-md">{text}
+    <button on:click={toggle} class=" px-5 py-2 rounded-lg bg-[#86868B] text-white hover:shadow-md">{text}
     </button>
-{#if _toggle}
-    <div class="absolute p-0 m-0 bg-white  " transition:fade >
-
-        <slot/>
-    </div>
-    {/if}
+    {#if _toggle}
+        <div class="absolute p-0 m-0 bg-white w-full" transition:fly >
+    
+            <slot/>
+        </div>
+        {/if}
 </div>
